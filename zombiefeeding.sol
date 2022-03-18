72064030da8ecedca0541c178fe497ba22507bf7
Patrick Bruso <pkbruso@gmail.com>
2022-03-18 11:15:22 -0700

    added code to check targetdna length, create new dna from average of zombie and target dna, and call createzombie function

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -12,5 +12,14 @@ contract ZombieFeeding is ZombieFactory {
 
         // Create new Zombie on storage and set it equal to Zombie from zombies array
         Zombie storage myZombie = zombies[_zombieId];
+
+        // Check to make sure target dna is not longer than 16 digits
+        _targetDna = _targetDna % dnaModulus;
+
+        // Create new dna variable to hold average of zombie dna and target dna
+        uint newDna = (myZombie.dna + _targetDna) / 2;
+
+        // Call createZombie function with new dna parameters and no name
+        _createZombie("NoName", newDna);
     }
 }
