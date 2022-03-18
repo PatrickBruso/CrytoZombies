a78b228c2bc80bbcbb97f7d33b8fc14a06fc7d42
Patrick Bruso <pkbruso@gmail.com>
2022-03-17 17:11:04 -0700

    created function to multiply zombie after require check for owner

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -4,4 +4,13 @@ import "./zombiefactory.sol";
 
 contract ZombieFeeding is ZombieFactory {
     
+    // Create function to feed and multiply zombies
+    function feedAndMultiply(uint _zombieId, uint _targetDna) public view {
+
+        // Use require to make sure msg.sender owns this zombie to feed
+        require(msg.sender == zombieToOwner[_zombieId]);
+
+        // Create new Zombie on storage and set it equal to Zombie from zombies array
+        Zombie storage myZombie = zombies[_zombieId];
+    }
 }
