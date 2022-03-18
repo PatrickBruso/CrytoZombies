1ed57829e810d65f4c00072d8b9efdce56e04623
Patrick Bruso <pkbruso@gmail.com>
2022-03-18 12:13:07 -0700

    created interface to use getKitty function from CryptoKitties smart contract

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -2,6 +2,22 @@ pragma solidity >=0.5.0 <0.6.0;
 
 import "./zombiefactory.sol";
 
+// Create interface to use getKitty function from CryptoKitties smart contract
+contract KittyInterface {
+    function getKitty(uint256 _id) external view returns (
+        bool isGestating,
+        bool isReady,
+        uint256 cooldownIndex,
+        uint256 nextActionAt,
+        uint256 siringWithId,
+        uint256 birthTime,
+        uint256 matronId,
+        uint256 sireId,
+        uint256 generation,
+        uint256 genes
+    );
+}
+
 contract ZombieFeeding is ZombieFactory {
     
     // Create function to feed and multiply zombies
